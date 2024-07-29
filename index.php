0cf5c65ae72909a227a606dfbb458735ad6754b5
tomas <tomas.hedera@centrum.cz>
2024-07-29 22:05:59 +0200

    responsive registr form

--- index.php
+++ index.php
@@ -1,51 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="./css/reset.css">
 	<link rel="stylesheet" type="text/css" href="./css/style.css">
-	
+
 	<title></title>
 </head>
+
 <body>
+<!-- tlacitka registrovat a prihlasit-->
 	<div class="prihlaseni">
-	
+
 		<button name="registrovat" id="registrBtn">Registrovat</button>
 		<br>
 		<button name="prihlasit" id="sign-inBtn">Přihlásit</button>
 
 	</div>
-
+<!-- kontaktni formular prijizdejici zleva-->
 	<div id="formContainer">
 		<form method="post" id="registerForm">
 			<h2>Registrace</h2>
 			<div>
-			<label for="jmeno">Jméno</label>
-			<input type="text" name="jmeno" id="jmeno">
+				<label for="jmeno">Jméno:</label>
+				<input type="text" name="jmeno" id="jmeno">
 			</div>
-			
+
 			<div>
-			<label for="prijmeni">Příjmení</label>
-			<input type="text" name="prijmeni" id="prijmeni">
+				<label for="prijmeni">Příjmení:</label>
+				<input type="text" name="prijmeni" id="prijmeni">
 			</div>
-			
+
 			<div>
-			<label for="email">Email</label>
-			<input type="email" name="emaill" id="email">
+				<label for="email">Email:</label>
+				<input type="email" name="emaill" id="email">
 			</div>
-			
+
 			<div>
-			<label for="heslo">Heslo</label>
-			<input type="password" name="heslo" id="heslo">
+				<label for="heslo">Heslo:</label>
+				<input type="password" name="heslo" id="heslo">
 			</div>
-			
+
 
 			<button name="registrovat">Registrovat</button>
 		</form>
 	</div>
 	</div>
 
+<!-- kontaktni formular prijizdejici zprava-->
+ 
+
+
 	<script src="./js/index.js"></script>
 </body>
+
 </html>
