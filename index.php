8eede0f411492684c87ff888500c4579ebfe5f5d
tomas <tomas.hedera@centrum.cz>
2024-08-03 10:09:04 +0200

    validate form

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
+<?php
+$jmeno = "";
+$email = "";
+$telefoniCislo = "";
+$spolecnost = "";
+$heslo = "";
+$hesloZnova = "";
+$chyba = [];
+$formVisible = false;
+
+
+
+	
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="./css/reset.css">
 	<link rel="stylesheet" type="text/css" href="./css/style.css">
-
 	<title></title>
 </head>
 
@@ -25,27 +38,47 @@
 			<button id="closeRegister" class="closeBtn">X</button>
 			<h2>Registrace</h2>
 			<div>
-				<label for="jmeno">Jméno:</label>
-				<input type="text" name="jmeno" id="jmeno">
+				<label for="jmeno">Jméno a příjmení:</label>
+				<input type="text" name="jmeno" id="jmeno" required>
 			</div>
-
-			<div>
-				<label for="prijmeni">Příjmení:</label>
-				<input type="text" name="prijmeni" id="prijmeni">
-			</div>
+			<span class="error" id="errorJmeno"></span>
 
 			<div>
 				<label for="email">Email:</label>
-				<input type="email" name="emaill" id="emailRegistr">
+				<input type="email" name="email" id="emailRegistr" required>
 			</div>
+			<span class="error" id="errorEmail"></span>
+
+			<div>
+				<label for="telcislo">Telefoní číslo:</label>
+				<input type="text" name="telcislo" id="telcislo" required>
+			</div>
+			<span class="error" id="errorTelcislo"></span>
+
+			<div>
+				<label for="spolecnost">Název společnosti:</label>
+				<input type="text" name="spolecnost" id="spolecnost" required>
+			</div>
+			<span class="error" id="errorSpolecnost"></span>
+
 
 			<div>
 				<label for="heslo">Heslo:</label>
-				<input type="password" name="heslo" id="hesloRegistr">
+				<input type="password" name="heslo" id="hesloRegistr" required>
 			</div>
+			<span class="error" id="errorHeslo"></span>
 
 
-			<button name="registrovat">Registrovat</button>
+			<div>
+				<label for="hesloznova">Potvrzení hesla:</label>
+				<input type="password" name="hesloznova" id="hesloRegistrZnova" required>
+			</div>
+			<span class="error" id="errorHesloznova"></span>
+
+
+
+
+			<button name="zaregistrovat">Registrovat</button>
 		</form>
 	</div>
 	</div>
@@ -58,13 +91,16 @@
 			<h2>Přihlášení</h2>
 			<div>
 				<label for="email">Email:</label>
-				<input type="email" name="email" id="emailSignin">
+				<input type="email" name="email" id="emailSignin" required>
 			</div>
+			<span class="error" id="errorEmail"></span>
 
 			<div>
 				<label for="heslo">Heslo:</label>
-				<input type="password" name="heslo" id="hesloSignin">
+				<input type="password" name="heslo" id="hesloSignin" required>
 			</div>
+			<span class="error" id="errorHeslo"></span>
+
 
 			 <button name="prihlasit">Přihlásit</button>
 		</form>
--- js/index.js
+++ js/index.js
@@ -1,27 +1,85 @@
 document.addEventListener('DOMContentLoaded', function(){
-	const registrBtn = document.getElementById('registrBtn');
-	const formContainer = document.getElementById('formContainer');
-	const signinBtn = document.getElementById('signinBtn');
-	const formContainerRight = document.getElementById('formContainerRight');
-	const closeRegister = document.getElementById('closeRegister');
-	const closeSignin = document.getElementById('closeSignin');
+    const registrBtn = document.getElementById('registrBtn');
+    const formContainer = document.getElementById('formContainer');
+    const signinBtn = document.getElementById('signinBtn');
+    const formContainerRight = document.getElementById('formContainerRight');
+    const closeRegister = document.getElementById('closeRegister');
+    const closeSignin = document.getElementById('closeSignin');
 
-	registrBtn.addEventListener('click', function(){
-		formContainer.classList.add('visible');
+
+	//validace
+
+	function validateForm()
+	{
+		let isValid = true;
+
+		//vycistí  staré chybové hlášky
+		document.querySelectorAll('error').forEach(error =>{
+			error.textContent = '';
+		});
+
+		//kontrola jmena
+		document.getElementById('jmeno').value.trim();
+		if(jmeno == '')
+		{
+			document.getElementById('errorJmeno').textContent = 'Jméno je povinné';
+			isValid = false
+		}
+
+
+		//kontrola emailu
+		const email = document.getElementById('emailRegistr').value.trim();
+		if(email == '')
+		{
+			document.getElementById('errorEmail').textContent = 'Email je povinný';
+			isValid = false;
+		}
+		else if (!validateEmail(email))
+		{
+			document.getElementById('errorEmail').textContent = 'Email není platný';
+			isValid = false
+		}
+
+		//kontrola telefoniho cisla
+		const telcislo = document.getElementById('telcislo').value.trim();
+		if(telcislo == '')
+		{
+			document.getElementById('errorTelcislo').textContent = 'Telefonní číslo je povinný';
+			isValid = false;
+		}
+
+		
+	}
+
+
+
+
+
+
+
+    registrBtn.addEventListener('click', function(){
+       if(!formContainer.classList.contains('visible'))
+	   {
+			formContainerRight.classList.remove('visibleRight');
+			formContainer.classList.add('visible');
+	   }
+    });
+
+    signinBtn.addEventListener('click', function(){
+		if(!formContainerRight.classList.contains('visibleRight'))
+		{
+			formContainer.classList.remove('visible');
+			formContainerRight.classList.add('visibleRight');
+		}
 	});
 
-	signinBtn.addEventListener('click', function(){
-		formContainerRight.classList.add('visibleRight');
-	});
+    closeRegister.addEventListener('click', function() {
+        formContainer.classList.remove('visible');
+    });
 
-	closeRegister.addEventListener('click', function() {
-		formContainer.classList.remove('visible');
-	});
+    closeSignin.addEventListener('click', function(){
+        formContainerRight.classList.remove('visibleRight');
+    });
 
-	closeSignin.addEventListener('click', function(){
-		formContainerRight.classList.remove('visibleRight')
-	});
-
-
+	
 });
-
